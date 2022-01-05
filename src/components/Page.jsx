90440e6b37aeb2aca1fd6ea4eaf1dd0972a20430
marco90202 <marco90202@hotmail.com>
2022-01-05 13:55:27 -0500

    Se quitaron colores del navigation bar y se creo la primera card 05_01_22

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './header/Header'
+import Body from './body/Body'
 
 const Page = () => {
 
     return(
         <div>
             <Header></Header>
-
+            <Body></Body>
         </div>
     )
 }
---
+++ src/components/organisms/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react' 
+
+const Card = () => { 
+
+    /* LOGICA DE JAVASCRIPT */ 
+
+    return(
+        
+        <div>
+            <div className="cards-list">
+                <div className="card">
+                    <div className="card_image">
+                        <img src="https://delgadoabogados.pe/wp-content/uploads/2021/08/logo-1-transparente.png"  alt="errorimg"  />
+                    </div>
+                    <div className="card_title title-black">
+                    <p>Card Title</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+export default Card
