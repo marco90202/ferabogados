cad2bd6f359885d328d76455cd5a9ee11a39df1d
heyden1976 <heyden1976@hotmail.com>
2022-01-06 09:39:19 -0500

    cambios

---
+++ src/components/body/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../organisms/Card'
+const Body = () => {
+
+
+    return(
+        <div>
+            <Card></Card>
+        </div>
+    )
+}
+export default Body
---
+++ src/components/organisms/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Card =() => {
+
+
+    return(
+        <div>estoy en card</div>
+    )
+}
+export default Card
