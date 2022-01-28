530f746f63cac65f09130b7af37ee080db9053a8
Joselo91 <jl_romero_v@hotmail.com>
2022-01-28 13:52:36 -0500

    guardar cambios

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -14,21 +14,21 @@ const Contact = () => {
                 <section className='formulario-1'>
                     <h2>formulario registro</h2>
                            <div className='box'>
-                            <input type="text" className='controls'placeholder='nombre' />
+                            <input type="text" className='controls' placeholder='nombre'/>
                             
-                            <input type="text" className='controls'placeholder='apellido'/>
+                            <input type="text" className='controls' placeholder='apellido'/>
                              </div>
-                            <input type="email" className='controls'placeholder='correo electronico'/>
+                            <input type="email" className='controls' placeholder='correo electronico'/>
                         
-                            <input type="tel" className='controls'placeholder='telefono'/>
+                            <input type="tel" className='controls' placeholder='telefono'/>
                             <div className='box-1'>
-                            <input type="text" className='controls'placeholder='empresa'/>
+                            <input type="text" className='controls' placeholder='empresa'/>
                             
-                            <input type="text" className='controls'placeholder='cargo'  />
+                            <input type="text" className='controls' placeholder='cargo'  />
                            </div>
-                            <input type="text" className='controls'placeholder='mensaje'/>
-                            <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
-                            <input type="submit"  className='botons'value='registrar' />
+                            <input type="text" className='controls' placeholder='mensaje'/>
+                            <p>Estoy de acuerdo con <a href="/">Terminos y Condiciones</a></p>
+                            <input type="submit"  className='botons' value='registrar' />
                         
                 </section>
             </body>
