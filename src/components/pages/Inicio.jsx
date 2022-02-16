784e500c306ac872c0f574f3cdff65158475b2ae
Joselo91 <jl_romero_v@hotmail.com>
2022-02-16 11:02:34 -0500

    Cambios misión y visión

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -11,13 +11,16 @@ const Inicio = () => {
                 <h1>Ferabogados Gestion Publica y Empresarial</h1>
                 <img src= 'https://i.pinimg.com/474x/bd/b6/8f/bdb68fa3091500b3249bbb37a2faf56e.jpg' alt='derecho-logo'/>
                 <div className='text'>
+                <h2>QUIENES SOMOS</h2>
+                <p>Somos una entidad educativa que desde el año 2020 asiste conﬁdencial y eﬁcientemente al público en general, estudiantes, empresas en su educación, capacitación, expansión y desarrollo a nivel local y regional, siendo nuestra visión ser líderes en servicios de capacitación empresarial y profesional.
+Una de nuestras metas es la promoción de la excelencia e innovación de los procesos de gestión y programas de capacitación, para así lograr ser socios estratégicos.</p>
                     <h2>MISION</h2>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo totam, dolorem ratione nisi ipsum deleniti obcaecati? Ad, officiis saepe neque reiciendis id quisquam exercitationem veniam autem at vitae minus error est, tempora illo tempore, odio fuga dolore maiores dolores nulla possimus culpa. Accusamus corrupti, voluptatibus consequuntur tempore dolore hic! Saepe!</p>
+                        <p>Formar profesionales, a nivel de especialidad, líderes íntegros e innovadores con visión global y con sentido crítico basados en los principales valores que contribuyan al bienestar en las organizaciones y la sociedad.</p>
                             <br></br>
                     <h2>VISION</h2>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo totam, dolorem ratione nisi ipsum deleniti obcaecati? Ad, officiis saepe neque reiciendis id quisquam exercitationem veniam autem at vitae minus error est, tempora illo tempore, odio fuga dolore maiores dolores nulla possimus culpa. Accusamus corrupti, voluptatibus consequuntur tempore </p>
+                        <p>Ser al 2025 la mejor institución educativas autorizado por el Ministerio de Educación en brindar las más altas competencias profesionales promoviendo el desarrollo integral en la transformación del Perú.</p>
                 </div>
-            </div> 
+            </div>
         </div>
         
     )
