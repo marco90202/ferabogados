5baa37957cf09f9a05ef4a54815f944ab87b1c55
heyden1976 <heyden1976@hotmail.com>
2022-03-18 09:59:31 -0500

    avance sidebar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,74 @@
-import React from 'react' 
-import logo from '../../assets/images/logo_web.png'
+import React from "react";
+import logo from "../../assets/images/logo_web.png";
 
 const Header = () => {
-
-
-    return(
-            <div className="padding">
-            <nav className="menu">
-                <a href="/"><img className="logo" src={logo} alt='FERABOGADOS' height='70' ></img> </a>
-                <ul className="menu_items">
-                    <li className="active"><a href="/inicio">inicio</a></li>
-                    <li><a href='/nosotros'>nosotros</a></li>
-                    <li><a href='/servicios'>servicios</a></li>
-                    <li><a href=" /eventos">eventos</a></li>
-                    <li><a href='/contacto'>contactenos</a></li>
-                    
-                </ul>
-            </nav>
+  return (
+    <div>
+      <nav className="menu">
+        <div className="container">
+          <div className="icon-hover">
+            |||
+            <div className="sidebar">
+             <ul className="cont_sidebar">
+               <li>inicio</li>
+               <li>nosotros</li>
+               <li>servicios</li>
+               <li>eventos</li>
+               <li>contactenos</li>
+             </ul>
             </div>
-            
-    )
-}
-export default Header
+          </div>
+
+          <a href="/">
+            <img
+              className="logo"
+              src={logo}
+              alt="FERABOGADOS"
+              height="70"
+            ></img>{" "}
+          </a>
+        </div>
+        <ul className="menu_items">
+          <li className="active">
+            <a href="/inicio">inicio</a>
+          </li>
+          <li>
+            <a href="/nosotros">nosotros</a>
+          </li>
+          <li>
+            <a href="/servicios">servicios</a>
+            <ul className="subitems">
+              <li>
+                <a href="#">subitem</a>
+              </li>
+              <li>
+                <a href="#">subitem</a>
+              </li>
+              <li>
+                <a href="#">subitem</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href=" /eventos">eventos</a>
+            <ul className="subitems">
+              <li>
+                <a href="#">subitem</a>
+              </li>
+              <li>
+                <a href="#">subitem</a>
+              </li>
+              <li>
+                <a href="#">subitem</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="/contacto">contactenos</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+export default Header;
